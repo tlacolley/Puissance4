6b537f94312cfeb509b30aca0fc9e1098b1de6ca
tlacolley <thomaslacolley@gmail.com>
2018-06-12 10:08:11 +0200

    Creation tableau JS et ajout de la section pour le future tableau

---
+++ script.js
@@ -0,0 +1,7 @@
+var launcher = [1,1,1,1,1,1,1];
+var body = document.getElementsByTagName("body")
+var matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
+
+
+var containerTab = document.createElement("section");
+body[0].appendChild(containerTab);
