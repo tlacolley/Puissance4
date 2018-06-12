3d7994b841ad7438cb804d5da490efce891c458c
gosset <dadgosset@orange.f>
2018-06-12 10:42:58 +0200

    travail sur js pour le tableau a revoir

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var launcher = [1,1,1,1,1,1,1];
 var body = document.getElementsByTagName("body")
 var matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
 
-
+var containerLaunch = document.createElement("section");
+body[0].appendChild(containerLaunch);
 var containerTab = document.createElement("section");
 body[0].appendChild(containerTab);
+
+
+// containerTab.appendChild(matrix);
+
+for (var i = 0; i < launcher.length; i++) {
+    containerLaunch.appendChild(launcher[i]);
+}
